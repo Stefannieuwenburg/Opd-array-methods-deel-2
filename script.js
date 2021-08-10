eef78f8b42dc95daace2bacce0eb9fd19f8f1dd7
Stefan Nieuwenburg <nieuwenburg.stefan@gmail.com>
2021-08-10 14:21:56 +0200

    Aan vulling javascript opd.

---
+++ script.js
@@ -0,0 +1,45 @@
+//Opd A
+const superheroes = [
+    {name: "Batman", alter_ego: "Bruce Wayne"}, 
+    {name: "Superman", alter_ego: "Clark Kent"}, 
+    {name: "Spiderman", alter_ego: "Peter Parker"}]
+    
+    // Hier komt jouw functie
+    
+    console.log(findSpiderMan(superheroes)) 
+    // Find Spiderman
+    // result should be: {name: "Spiderman", alter_ego: "Peter Parker"}
+
+    //Opd B
+    doubleArrayValues([1, 2, 3]) 
+    // result should be [2, 4, 6]
+
+    //Opd C
+    containsNumberBiggerThan10([1, 4, 3, 6, 9, 7, 11]) 
+
+    // result should be true
+    containsNumberBiggerThan10([1,2,1,2,1,2])
+
+    // result should be false
+
+    //Opd D
+    isItalyInTheGreat7(['Canada', 'France','Germany','Italy','Japan','United Kingdom','United States']) 
+    // result should be true
+
+
+
+    //Opd E
+    console.log(tenfold([1, 4, 3, 6, 9, 7, 11]))
+  // result should be [10, 40, 30, 60, 90, 70, 110]
+
+
+
+  //Opd F
+  console.log(isBelow100([1, 81, 4, 53, 3, 6, 79, 2, 43, 7, 28, 101, 11, 77, 84, 98 ]))
+  // result should be: false
+
+
+
+  // Opd G
+  console.log(bigSum([1, 81, 4, 53, 3, 6, 79, 2, 43, 7, 28, 11, 77, 84, 98, 101, 206, 234]))
+  // result should be 1118
